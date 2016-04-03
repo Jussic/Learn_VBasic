db031616c1d6989a04b53fccb5a60bdfd33a63f3
JUSSIC <justineclarkes@gmail.com>
2016-04-02 21:25:51 -0400

    VB_Tut: Timed Password
    Password opportunity expires if not entered in 15 seconds.

---
+++ TimedPassword/TimedPassword/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If TextBox1.Text = "secret" Then
+            Timer1.Enabled = False
+            MsgBox(" Welcome to the system")
+            End
+        Else
+            MsgBox("Sorry, friend, I dont know you")
+        End If
+    End Sub
+End Class
+
+' This is not working.....
