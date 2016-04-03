ce824053f543d7cafdba74d70f2e78066c75520f
JUSSIC <justineclarkes@gmail.com>
2016-04-02 21:27:22 -0400

    VB_Tut: Radio Button
    Radio Button Tutorial

---
+++ MyUserValidation/MyUserValidation/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Private Sub MaskedTextBox1_MaskInputRejected(sender As Object, e As MaskInputRejectedEventArgs) Handles MaskedTextBox1.MaskInputRejected
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If MaskedTextBox1.Text = "555-55-1212" And MaskedTextBox2.Text = "54321" Then
+            MsgBox("Welcome to the System")
+        Else
+            MsgBox("I do not recognize this number")
+        End If
+    End Sub
+
+    Private Sub MaskedTextBox2_MaskInputRejected(sender As Object, e As MaskInputRejectedEventArgs) Handles MaskedTextBox2.MaskInputRejected
+
+    End Sub
+End Class
